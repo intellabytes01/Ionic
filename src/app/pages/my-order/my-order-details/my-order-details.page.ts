1c6e1fe9831ab9105cf78848b8a5b04b5bcd1325
Abhijeetu <abhijeetu@pharmarack.com>
2019-07-03 12:41:22 +0530

    - Added ngOnDestroy method. - Called UntilDestroyed method inside pipe.

--- src/app/pages/my-order/my-order-details/my-order-details.page.ts
+++ src/app/pages/my-order/my-order-details/my-order-details.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { map } from 'rxjs/operators';
@@ -10,7 +10,7 @@ import { untilDestroyed } from '@app/core';
   templateUrl: './my-order-details.page.html',
   styleUrls: ['./my-order-details.page.scss']
 })
-export class MyOrderDetailsPage implements OnInit {
+export class MyOrderDetailsPage implements OnInit, OnDestroy {
   state$: Observable<object>;
   orderDetails: any = {};
   constructor(public activatedRoute: ActivatedRoute, public iab: InAppBrowser) {
@@ -18,16 +18,21 @@ export class MyOrderDetailsPage implements OnInit {
   }
 
   ngOnInit() {
-    this.state$ = this.activatedRoute.paramMap.pipe(map(() => window.history.state));
+    this.state$ = this.activatedRoute.paramMap.pipe(
+      untilDestroyed(this), map(() => window.history.state));
     this.state$.subscribe((data: any) => {
       if (data.data) {
         this.orderDetails = JSON.parse(data.data);
       }
-    }),
-    untilDestroyed(this);
+    });
   }
 
   call() {
     this.iab.create(`tel:${this.orderDetails.mobilenumber}`);
+  }
+
+  ngOnDestroy(): void {
+    // Called once, before the instance is destroyed.
+    // Add 'implements OnDestroy' to the class.
   }
 }
