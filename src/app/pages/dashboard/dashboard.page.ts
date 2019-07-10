2492d7a311cf7e22959cabc58197194042a482de
infoxen_dev <devcenter@infoxen.com>
2019-07-10 15:20:31 +0530

    10/7 register error fix and hardware back button handle on dashboard

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { MenuController, Platform, AlertController } from '@ionic/angular';
 import { Page } from './interface/dashboard';
 import { TranslateService } from '@ngx-translate/core';
 import { UtilityService } from '@app/shared/services/utility.service';
@@ -21,17 +21,29 @@ import { NewOrderService } from '../Retailer/new-order/new-order.service';
 export class DashboardPage implements OnInit, OnDestroy {
   pages = [];
   userData: object;
+  backButton: any;
 
   constructor(public menuCtrl: MenuController,
               private translateService: TranslateService,
               private utilityService: UtilityService,
               private store: Store<AuthState>,
               private storage: Storage,
-              private newOrderService: NewOrderService) {}
+              private newOrderService: NewOrderService,
+              private platform: Platform,
+              public alertController: AlertController) {}
 
   ionViewWillEnter() {
     this.menuCtrl.enable(true, 'menuLeft');
     this.menuCtrl.enable(true, 'menuRight');
+    this.backButton = this.platform.backButton.subscribeWithPriority(9999, () => {
+      this.presentAlertConfirm();
+    });
+  }
+
+  ionViewDidLeave() {
+    if (this.backButton) {
+      this.backButton.unsubscribe();
+   }
   }
 
   ngOnInit() {
@@ -91,9 +103,31 @@ export class DashboardPage implements OnInit, OnDestroy {
     }
   }
 
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: '',
+      message: '<br><strong>Are you sure you want to exit the app?</strong>',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            navigator['app'].exitApp();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
   ngOnDestroy(): void {
     // Called once, before the instance is destroyed.
     // Add 'implements OnDestroy' to the class.
-
   }
 }
